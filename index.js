18c78719e954180e3f4e5261a64afa3babe6fa93
kmui2 <kmui2@wisc.edu>
2017-11-01 00:07:28 -0500

    categories count method

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const bodyParser = require('body-parser');
 const csv = require('csvtojson');
 
 let app = express();
-let writer = csvWriter({sendHeaders: false});
+let writer = csvWriter({ sendHeaders: false });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('port', (process.env.PORT || 7072))
 
 // Add headers
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
 // For Rendering HTML
@@ -35,31 +35,44 @@ app.listen(app.get('port'), function () {
   console.log("Node app is running at http://localhost:" + app.get('port'))
 })
 
-let categoriesQueues = {};
 let images = {};
+let categoriesCount = {};
 fs.readdirSync('dev/17-objects').forEach(folder => {
   if (folder != '.DS_Store') {
+    categoriesCount[folder] = 0;
     images[folder] = [];
-    fs.readdirSync('dev/17-objects/'+folder).forEach(file => {
+    fs.readdirSync('dev/17-objects/' + folder).forEach(file => {
       if (file == 'TestItems') {
-        fs.readdirSync('dev/17-objects/'+folder+'/TestItems').forEach(file => {     
-          if (!['.DS_Store', 'Thumbs.db', 'extra','AMC5143AAS_COB_370.jpe'].includes(file)) {
-            images[folder].push('17-objects/'+folder+'/TestItems/'+file);
+        fs.readdirSync('dev/17-objects/' + folder + '/TestItems').forEach(file => {
+          if (!['.DS_Store', 'Thumbs.db', 'extra', 'AMC5143AAS_COB_370.jpe'].includes(file)) {
+            images[folder].push('17-objects/' + folder + '/TestItems/' + file);
           }
         });
       }
-      if (!['.DS_Store', 'TestItems', 'Thumbs.db', 'extra','AMC5143AAS_COB_370.jpe'].includes(file)) {
+      if (!['.DS_Store', 'TestItems', 'Thumbs.db', 'extra', 'AMC5143AAS_COB_370.jpe'].includes(file)) {
         // console.log('dev/17-objects/'+folder+'/'+file)
-        images[folder].push('17-objects/'+folder+'/'+file);
+        images[folder].push('17-objects/' + folder + '/' + file);
       }
     })
   }
 });
+let categories = Object.keys(categoriesCount);
+
+
+let catPath = 'categoriesCount.csv';
+let headers = categories;
+if (fs.existsSync(catPath))
+  fs.unlinkSync(catPath);
+writer = csvWriter({ headers: headers });
+
+writer.pipe(fs.createWriteStream(catPath, { flags: 'a' }));
+writer.write(categoriesCount);
+writer.end();
+
 // POST endpoint for requesting trials
 app.post('/trials', function (req, res) {
   console.log("trials post request received");
-  
-  let categories = [];
+
   let subjCode = req.body.subjCode;
   let numTrials = req.body.numTrials;
   let newSet = req.body.newSet;
@@ -68,97 +81,145 @@ app.post('/trials', function (req, res) {
   console.log("numTrials received is " + numTrials);
   console.log("newSet received is " + newSet);
   console.log("reset received is " + newSet);
-  if (reset == 'false' && fs.existsSync('trials/'+subjCode+'_trials.txt')) {
+  if (reset == 'false' && fs.existsSync('trials/' + subjCode + '_trials.txt')) {
     console.log('Grabbing unfinished trials')
     let completed = [];
-    const csvFilePath='data/'+subjCode+'_data.csv';
+    const csvFilePath = 'data/' + subjCode + '_data.csv';
     csv()
-    .fromFile(csvFilePath)
-    .on('json',(jsonObj)=>{
+      .fromFile(csvFilePath)
+      .on('json', (jsonObj) => {
         // combine csv header row and csv line to a json object
         // jsonObj.a ==> 1 or 4
         completed.push(jsonObj.category);
-    })
-    .on('done',(error)=>{
-      fs.readFile('trials/'+subjCode+'_trials.txt', 'utf8', function(err, data) {
-        if (err) throw err;
-        let trialsList = data.split('\n');
-        let subjCategories = [];
-        for (let c of trialsList)
-          if (!completed.includes(c))
-            subjCategories.push(c);
-        
-        let subjImages = Object.assign({}, images); 
-        for (let category in subjImages)
-          subjImages[category] = _.shuffle(subjImages[category]);
-        
-        let path = 'IRQ_questions.txt';
-        let questions = _.shuffle(fs.readFileSync(path).toString().replace(/\r/g,'\n').split('\n')).filter((line) => {return line.replace(/ /g,'').length>0});
-        let trials = {categories: subjCategories, images: subjImages, questions: questions};
-        
-        res.send({success: true, trials: trials});
-          
-      });
-    })
+      })
+      .on('done', (error) => {
+        fs.readFile('trials/' + subjCode + '_trials.txt', 'utf8', function (err, data) {
+          if (err) throw err;
+          let trialsList = data.split('\n');
+          let subjCategories = [];
+          for (let c of trialsList)
+            if (!completed.includes(c))
+              subjCategories.push(c);
+
+          let subjImages = Object.assign({}, images);
+          for (let category in subjImages)
+            subjImages[category] = _.shuffle(subjImages[category]);
+
+          let path = 'IRQ_questions.txt';
+          let questions = _.shuffle(fs.readFileSync(path).toString().replace(/\r/g, '\n').split('\n')).filter((line) => { return line.replace(/ /g, '').length > 0 });
+          let trials = { categories: subjCategories, images: subjImages, questions: questions };
+
+          res.send({ success: true, trials: trials });
+
+        });
+      })
   }
   else {
-    if (fs.existsSync('trials/'+subjCode+'_trials.txt'))
-      fs.unlinkSync('trials/'+subjCode+'_trials.txt');
-    if (fs.existsSync('data/'+subjCode+'_data.csv'))
-      fs.unlinkSync('data/'+subjCode+'_data.csv');
-    if (newSet == 'true' || !(numTrials in categoriesQueues) || categoriesQueues[numTrials].length == 0 ) {
-      categoriesQueues[numTrials] = [];
-      images = {};
-      let categoriesQueue = [];
-      // include all images recursively in 17-objects directory
-      fs.readdirSync('dev/17-objects').forEach(folder => {
-        if (folder != '.DS_Store') {
-          categoriesQueue.push(folder);
-          images[folder] = [];
-          fs.readdirSync('dev/17-objects/'+folder).forEach(file => {
-            if (file == 'TestItems') {
-              fs.readdirSync('dev/17-objects/'+folder+'/TestItems').forEach(file => {     
-                if (!['.DS_Store', 'Thumbs.db', 'extra','AMC5143AAS_COB_370.jpe'].includes(file)) {
-                  images[folder].push('17-objects/'+folder+'/TestItems/'+file);
-                }
-              });
-            }
-            if (!['.DS_Store', 'TestItems', 'Thumbs.db', 'extra','AMC5143AAS_COB_370.jpe'].includes(file)) {
-              // console.log('dev/17-objects/'+folder+'/'+file)
-              images[folder].push('17-objects/'+folder+'/'+file);
-            }
-          })
+    console.log('Creating new trials');
+    if (fs.existsSync('trials/' + subjCode + '_trials.txt'))
+      fs.unlinkSync('trials/' + subjCode + '_trials.txt');
+    if (fs.existsSync('data/' + subjCode + '_data.csv'))
+      fs.unlinkSync('data/' + subjCode + '_data.csv');
+
+    let currCount = {};
+    // csv()
+    // .fromFile(catPath)
+    // .on('json', (jsonObj) => {
+      // combine csv header row and csv line to a json object
+      // jsonObj.a ==> 1 or 4
+      // completed.push(jsonObj.category);
+      // currCount = jsonObj;
+    // })
+    // .on('done', (error) => {
+      
+        // if (error) throw error;
+
+        // console.log('currCount')
+        // console.log(currCount)
+
+        /*
+          { '0':
+            [ 'abacus',
+              'airplane',
+              'apple',
+              'armyguy',
+              'axe',
+              'babushkadolls',
+              'babycarriage',
+              'backpack',
+              'bagel',
+              'ball',
+        */
+        let countLists = {};
+        for (let cat in categoriesCount) {
+          if (!(categoriesCount[cat] in countLists))
+            countLists[categoriesCount[cat]] = [cat];
+          else 
+            countLists[categoriesCount[cat]].push(cat);
         }
-      });
-      while (categoriesQueue.length > 0) {
-        let chunk = [];
-        for (let i = 0; i < numTrials; i++) {
-          if (categoriesQueue.length == 0)
-            break;
-          chunk.push(categoriesQueue.pop());
+        // console.log('countLists: ')
+        // console.log(countLists);
+        // console.log('countLists[0]: ')
+        // console.log(countLists[0])
+        let counts = Object.keys(countLists).sort();
+        // console.log('counts:')
+        // console.log(counts)
+        let subjCategories = [];
+        for (let count of counts) {
+          // console.log('count: ')
+          // console.log(count)
+          // console.log('countLists['+count+']: ')
+          // console.log(countLists[count])
+          // console.log(countLists)
+            for (let cat of countLists[count]) {
+              if (subjCategories.length < numTrials) {
+                subjCategories.push(cat);
+                // console.log('cat: ')
+                // console.log(cat)
+                // console.log('categoriesCount[cat]: ')
+                // console.log(categoriesCount[cat])
+                categoriesCount[cat] = (Number(categoriesCount[cat]) + 1 )+'';
+              }
+              else
+                break;
+            }
         }
-        categoriesQueues[numTrials].push(chunk);
-      }
-    };
-    let subjCategories = _.shuffle(categoriesQueues[numTrials].pop());
-    
-    fs.writeFile('trials/'+subjCode+'_trials.txt', subjCategories.join('\n'), function(err) {
-      if(err) {
-          return console.log(err);
-      }
-      console.log("Trials list saved!");
-    }); 
-  
-    let subjImages = Object.assign({}, images); 
-    for (let category in subjImages)
-      subjImages[category] = _.shuffle(subjImages[category]);
-    
-    let path = 'IRQ_questions.txt';
-    let questions = _.shuffle(fs.readFileSync(path).toString().replace(/\r/g,'\n').split('\n')).filter((line) => {return line.replace(/ /g,'').length>0});
-    let trials = {categories: subjCategories, images: subjImages, questions: questions};
-    
-    console.log("Trial sets left for "+numTrials+" trials set: " +categoriesQueues[numTrials].length);
-    res.send({success: true, trials: trials});
+        
+
+        // let catPath = 'categoriesCount.csv';
+        // let headers = categories;
+        // if (!fs.existsSync(catPath))
+        //   writer = csvWriter({ headers: headers });
+        // else
+        //   writer = csvWriter({ sendHeaders: false });
+
+        // writer.pipe(fs.createWriteStream(catPath, { flags: 'a' }));
+        // // console.log('awkward')
+        // // console.log(categoriesCount);
+        // writer.write(categoriesCount);
+        // writer.end();
+
+
+        let subjImages = Object.assign({}, images);
+        for (let category in subjImages)
+          subjImages[category] = _.shuffle(subjImages[category]);
+
+        let path = 'IRQ_questions.txt';
+        let questions = _.shuffle(fs.readFileSync(path).toString().replace(/\r/g, '\n').split('\n')).filter((line) => { return line.replace(/ /g, '').length > 0 });
+        let trials = { categories: subjCategories, images: subjImages, questions: questions };
+
+        fs.writeFile('trials/' + subjCode + '_trials.txt', subjCategories.join('\n'), function (err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("Trials list saved!");
+        });
+        console.log(categoriesCount);
+        res.send({ success: true, trials: trials });
+
+    // })    
+    // let subjCategories = _.shuffle(categoriesQueues[numTrials].pop());
+
   }
 })
 // POST endpoint for receiving trial responses
@@ -168,16 +229,16 @@ app.post('/data', function (req, res) {
   // Parses the trial response data to csv
   let response = req.body;
   console.log(response);
-  let path = 'data/'+response.subjCode+'_data.csv';
+  let path = 'data/' + response.subjCode + '_data.csv';
   let headers = Object.keys(response);
   if (!fs.existsSync(path))
-    writer = csvWriter({ headers: headers});
+    writer = csvWriter({ headers: headers });
   else
-    writer = csvWriter({sendHeaders: false});
+    writer = csvWriter({ sendHeaders: false });
 
-  writer.pipe(fs.createWriteStream(path, {flags: 'a'}));
+  writer.pipe(fs.createWriteStream(path, { flags: 'a' }));
   writer.write(response);
   writer.end();
 
-  res.send({success: true});
+  res.send({ success: true });
 })
