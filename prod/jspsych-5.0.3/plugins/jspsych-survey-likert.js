58c14c7f745a21e4257395b926232ac94eb6f35e
kmui2 <kmui2@wisc.edu>
2017-11-01 01:54:40 -0500

    prod

--- prod/jspsych-5.0.3/plugins/jspsych-survey-likert.js
+++ prod/jspsych-5.0.3/plugins/jspsych-survey-likert.js
@@ -9,81 +9,89 @@
  */
 
 jsPsych.plugins['survey-likert'] = (function() {
-
-  var plugin = {};
-
-  plugin.trial = function(display_element, trial) {
-
-    // default parameters for the trial
-    trial.preamble = typeof trial.preamble === 'undefined' ? "" : trial.preamble;
-
-    // if any trial variables are functions
-    // this evaluates the function and replaces
-    // it with the output of the function
-    trial = jsPsych.pluginAPI.evaluateFunctionParameters(trial);
-
-    // show preamble text
-    display_element.append($('<div>', {
-      "id": 'jspsych-survey-likert-preamble',
-      "class": 'jspsych-survey-likert-preamble'
-    }));
-
-    $('#jspsych-survey-likert-preamble').html(trial.preamble);
-
-    display_element.append('<form id="jspsych-survey-likert-form">');
-    // add likert scale questions
-    for (var i = 0; i < trial.questions.length; i++) {
-      form_element = $('#jspsych-survey-likert-form');
-      // add question
-      form_element.append('<label class="jspsych-survey-likert-statement">' + trial.questions[i] + '</label>');
-      // add options
-      var width = 100 / trial.labels[i].length;
-      options_string = '<ul class="jspsych-survey-likert-opts" data-radio-group="Q' + i + '">';
-      for (var j = 0; j < trial.labels[i].length; j++) {
-        options_string += '<li style="width:' + width + '%"><input type="radio" name="Q' + i + '" value="' + j + '"  style="height:35px; width:35px; vertical-align: middle;"><label class="jspsych-survey-likert-opt-label">' + trial.labels[i][j] + '</label></li>';
-      }
-      options_string += '</ul>';
-      form_element.append(options_string);
-    }
-
-    // add submit button
-    display_element.append($('<button>', {
-      'id': 'jspsych-survey-likert-next',
-      'class': 'jspsych-survey-likert jspsych-btn'
-    }));
-    $("#jspsych-survey-likert-next").html('Submit Answers');
-    $("#jspsych-survey-likert-next").click(function() {
-      // measure response time
-      var endTime = (new Date()).getTime();
-      var response_time = endTime - startTime;
-
-      // create object to hold responses
-      var question_data = {};
-      $("#jspsych-survey-likert-form .jspsych-survey-likert-opts").each(function(index) {
-        var id = $(this).data('radio-group');
-        var response = $('input[name="' + id + '"]:checked').val();
-        if (typeof response == 'undefined') {
-          response = -1;
+  
+    var plugin = {};
+  
+    plugin.trial = function(display_element, trial) {
+  
+      // default parameters for the trial
+      trial.preamble = typeof trial.preamble === 'undefined' ? "" : trial.preamble;
+  
+      // if any trial variables are functions
+      // this evaluates the function and replaces
+      // it with the output of the function
+      trial = jsPsych.pluginAPI.evaluateFunctionParameters(trial);
+  
+      // show preamble text
+      display_element.append($('<div>', {
+        "id": 'jspsych-survey-likert-preamble',
+        "class": 'jspsych-survey-likert-preamble'
+      }));
+  
+      $('#jspsych-survey-likert-preamble').html(trial.preamble);
+  
+      display_element.append('<form id="jspsych-survey-likert-form">');
+      // add likert scale questions
+      for (var i = 0; i < trial.questions.length; i++) {
+        form_element = $('#jspsych-survey-likert-form');
+        // add question
+        form_element.append('<label class="jspsych-survey-likert-statement">' + trial.questions[i] + '</label>');
+        // add options
+        var width = 100 / trial.labels[i].length;
+        options_string = '<ul class="jspsych-survey-likert-opts" data-radio-group="Q' + i + '">';
+        for (var j = 0; j < trial.labels[i].length; j++) {
+          options_string += '<li style="width:' + width + '%"><input type="radio" name="Q' + i + '" value="' + j + '"  style="height:35px; width:35px; vertical-align: middle;"><label class="jspsych-survey-likert-opt-label">' + trial.labels[i][j] + '</label></li>';
         }
-        var obje = {};
-        obje[id] = response;
-        $.extend(question_data, obje);
+        options_string += '</ul>';
+        form_element.append(options_string);
+      }
+  
+      // add submit button
+      display_element.append($('<button>', {
+        'id': 'jspsych-survey-likert-next',
+        'class': 'jspsych-survey-likert jspsych-btn'
+      }));
+      $("#jspsych-survey-likert-next").html('Submit Answers');
+      $("#jspsych-survey-likert-next").click(function() {
+        // measure response time
+        var endTime = (new Date()).getTime();
+        var response_time = endTime - startTime;
+  
+        // create object to hold responses
+        var question_data = {};
+        var valid = true;
+        $("#jspsych-survey-likert-form .jspsych-survey-likert-opts").each(function(index) {
+          var id = $(this).data('radio-group');
+          var response = $('input[name="' + id + '"]:checked').val();
+          if (typeof response == 'undefined') {
+            response = -1;
+            valid = false;
+          }
+          var obje = {};
+          obje[id] = response;
+          $.extend(question_data, obje);
+        });
+  
+        if (!valid) {
+          alert("Answer must be filled out");
+          return false;
+        }
+  
+        // save data
+        var trial_data = {
+          "rt": response_time,
+          "responses": JSON.stringify(question_data)
+        };
+  
+        display_element.html('');
+  
+        // next trial
+        jsPsych.finishTrial(trial_data);
       });
-
-      // save data
-      var trial_data = {
-        "rt": response_time,
-        "responses": JSON.stringify(question_data)
-      };
-
-      display_element.html('');
-
-      // next trial
-      jsPsych.finishTrial(trial_data);
-    });
-
-    var startTime = (new Date()).getTime();
-  };
-
-  return plugin;
-})();
+  
+      var startTime = (new Date()).getTime();
+    };
+  
+    return plugin;
+  })();
+  
