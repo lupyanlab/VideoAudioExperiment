50e05138c0dceb7ebe7a958ff7515822089dc94d
kmui2 <kmui2@wisc.edu>
2017-11-01 01:40:46 -0500

    consent form

--- dev/experiment.js
+++ dev/experiment.js
@@ -15,6 +15,30 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
         hitId: hitId
     });
 
+    // sample function that might be used to check if a subject has given
+    // consent to participate.
+    var check_consent = function(elem) {
+        if ($('#consent_checkbox').is(':checked')) {
+        return true;
+        }
+        else {
+        alert("If you wish to participate, you must check the box next to the statement 'I agree to participate in this study.'");
+        return false;
+        }
+        return false;
+    };
+    
+    
+    // declare the block.
+    var consent = {
+        type:'html',
+        url: "external_page.html",
+        cont_btn: "start",
+        check_fn: check_consent
+    };
+
+    timeline.push(consent);
+
     let welcome_block = {
         type: "text",
         cont_key: ' ',
