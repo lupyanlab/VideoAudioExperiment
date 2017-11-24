0e210a24c4ab43544b550d98bf0cfd9b7999ead5
kmui2 <kmui2@wisc.edu>
2017-11-24 12:26:12 -0600

    prod

--- prod/prod.js
+++ prod/prod.js
@@ -13,41 +13,23 @@ $(document).ready(function(){
         // DEFINE workerId, hitId, assignmentId HERE
         //////////////////////////////////////////
         let subjCode = $.urlParam('workerId') || 'unknown';
-        let numTrials = $.urlParam("numTrials") || 40;
-        let reset =  $.urlParam("newSet") || 'true';
-        let workerId = 'workerId';
-        let assignmentId = 'assignmentId';
-        let hitId = 'hitId';
+        let workerID = 'workerId';
 
         $("#loading").html('<h2 style="text-align:center;">Loading trials... please wait.</h2> </br> <div  class="col-md-2 col-md-offset-5"><img src="img/preloader.gif"></div>')
         
-        // This calls server to run python generate trials (judements.py) script
-        // Then passes the generated trials to the experiment
         $.ajax({
             url: 'http://'+document.domain+':'+PORT+'/trials',
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify({subjCode, numTrials, reset}),
+            data: JSON.stringify({subjCode, sessionID}),
             success: function (data) {
                 console.log(data);
                 
-                let images = [];
-                let categories = data.trials.categories;
-                let stimuli = data.trials.images;
-
-                for (let category of categories) {
-                    for (let file of stimuli[category]) {
-                        images.push(file);
-                    }
-                }
-                $("#loading").remove();
-                runExperiment(data.trials, subjCode, workerId, assignmentId, hitId);
-                jsPsych.pluginAPI.preloadImages(images, function(){}); 
-                    
                 
-                // });
-            }
-        })
+                // jsPsych.pluginAPI.preloadImages(images, function(){}); 
+                runExperiment(data.trials, subjCode, data.questions, workerId, assignmentId, hitId);
     
+            }
+        })    
 
 });
